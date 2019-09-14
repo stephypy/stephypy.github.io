7fde82a8a651b99ed73bbf63d4352b07c1ca9d12
Stephanie <55038687+stephypy@users.noreply.github.com>
2019-09-14 15:38:36 -0600

    Create toolbar.ts

---
+++ toolbar.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+/**
+ * Toolbar 
+ */
+@Component({
+  selector: 'toolbar-main',
+  templateUrl: 'toolbar.html',
+  styleUrls: ['toolbar.css'],
+})
+export class ToolbarMain {}
