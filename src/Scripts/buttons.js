a241c1d5a8917dff6c77f2fb7f36cb10b73575a7
Stephanie Galvan <55038687+stephypy@users.noreply.github.com>
2020-12-15 16:55:42 -0700

    responsive design for both portrait and landscape mode

--- src/Scripts/buttons.js
+++ src/Scripts/buttons.js
@@ -1,39 +1,20 @@
 window.onload = function() {
-  var currURL = window.location.href.split("/").pop();
+  const artBtn = document.getElementById("art-btn");
+  artBtn.addEventListener("click", function(){goToPage(artBtn.id)});
 
-  if (currURL == "index.html") {
-    const exploreButton = document.getElementById("explore");
-    exploreButton.addEventListener("click", function(){goToPage(exploreButton.id)});
-  }
-  else if (currURL == "explore.html") {
-    const index = document.getElementById("index");
-    index.addEventListener("click", function(){goToPage(index.id)});
-
-    const internships = document.getElementById("internships");
-    internships.addEventListener("click",  function(){goToPage(internships.id)});
-
-    const hackathons = document.getElementById("hackathons");
-    hackathons.addEventListener("click",  function(){goToPage(hackathons.id)});
-
-    const ugta = document.getElementById("ugta");
-    ugta.addEventListener("click",  function(){goToPage(ugta.id)});
-
-    const research = document.getElementById("research");
-    research.addEventListener("click",  function(){goToPage(research.id)});
-
-    const school = document.getElementById("school");
-    school.addEventListener("click",  function(){goToPage(school.id)});
-
-    const extracurricular = document.getElementById("extracurricular");
-    extracurricular.addEventListener("click",  function(){goToPage(extracurricular.id)});
-  }
-  else {
-    const goback = document.getElementById("explore");
-    goback.addEventListener("click", function(){goToPage(goback.id)});
-  }
+  const codeBtn = document.getElementById("code-btn");
+  codeBtn.addEventListener("click", function(){goToPage(codeBtn.id)});
 }
 
 /* Go to the page indicated by the button */
-function goToPage(pageName) {
-  window.location.replace(pageName + ".html");
+function goToPage(btn) {
+  switch(btn){
+    case "art-btn":
+      window.location.replace(btn + ".html");
+
+    case "code-btn":
+      window.location.replace(btn + ".html");
+
+    default:
+      window.location.replace(btn + ".html");
 }
