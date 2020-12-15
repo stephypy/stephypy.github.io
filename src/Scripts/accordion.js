b9cf42ed82be173813f1ea367c84b2ae09c319f7
Stephanie Galvan <55038687+stephypy@users.noreply.github.com>
2020-12-15 09:35:47 -0700

    wavy text animation in css

--- src/Scripts/accordion.js
+++
@@ -1,8 +0,0 @@
-const sections = document.getElementsByClassName("accordion");
-
-for (const section of sections) {
-  section.onclick = function() {
-    this.classList.toggle("active");
-    this.nextElementSibling.classList.toggle("show");
-  }
-}
